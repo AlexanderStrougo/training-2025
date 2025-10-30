22ea8f2e12cdcfbd32d57927284bf8ee44ddacf2
AlexanderStrougo <118131066+AlexanderStrougo@users.noreply.github.com>
2025-10-29 20:34:52 -0700

    update RobotContainer.java

--- tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,27 +1,37 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.shooter.ShooterIO.ShooterIOInputs;
 
 // Main Shooter subsystem logic
 public class Shooter extends SubsystemBase {
-  // TODO: Add a private field for your ShooterIO instance
-  // TODO: Add a private field for your ShooterIOInputs instance and initialize it
+  // Add a private field for your ShooterIO instance
+  private ShooterIO io;
+  // Add a private field for your ShooterIOInputs instance and initialize it
+  private final ShooterIO.ShooterIOInputs inputs = new ShooterIOInputs();
 
-  // TODO: Create the constructor that takes a ShooterIO instance as a parameter
-  // public Shooter(ShooterIO io) { ... }
+  // Create the constructor that takes a ShooterIO instance as a parameter
+  public Shooter(ShooterIO io) { this.io = io; }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    // TODO: Call the updateInputs method on your ShooterIO instance
+    // Call the updateInputs method on your ShooterIO instance
+    io.updateInputs(inputs);
   }
 
-  // TODO: Add a public method to spin the shooter motor at a given percentage output
-  // public void spin(double percent) { ... }
+  // Add a public method to spin the shooter motor at a given percentage output
+  public void spin(double percent) { 
+    io.setMotorPercentOutput(percent);
+  }
 
-  // TODO: Add a public method to stop the shooter motor
-  // public void stop() { ... }
+  // Add a public method to stop the shooter motor
+  public void stop() { 
+    io.setMotorPercentOutput(0);
+  }
 
-  // TODO: Add a public method to return the current motor RPM
-  // public double getCurrentRPM() { ... }
+  // Add a public method to return the current motor RPM
+  public double getCurrentRPM() { 
+    return inputs.motorRPM;
+  }
 }
