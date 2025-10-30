5602c6c5571dfc5d0be244002862f179fc011e27
AlexanderStrougo <118131066+AlexanderStrougo@users.noreply.github.com>
2025-10-29 20:27:17 -0700

    update ShooterIOSim

--- tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
+++ tasks/lesson3-shooter-task/src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
@@ -35,7 +35,7 @@ public class ShooterIOTalonFX implements ShooterIO {
 
   @Override
   public void setMotorPercentOutput(double percent) {
-    // TODO: Use motor.setControl and the dutyCycleOut variable to set the motor’s percent output
+    // Use motor.setControl and the dutyCycleOut variable to set the motor’s percent output
     // Tip: Search up documentation for DutyCycleOut methods
     motor.setControl(dutyCycleOut.withOutput(percent));
   }
